c519b61d8cbcbc7acea741337356be15dc39a789
rowleynt <116468465+rowleynt@users.noreply.github.com>
2023-03-18 00:59:48 -0400

    Update Month.py

--- Month.py
+++ Month.py
@@ -1,35 +1,26 @@
-from wecutils import map_to_name, map_to_alias, log_error
-
-
 class Month(object):
-    def __init__(self, month_num, event_dict=None):
-        #  month has the attributes: event_dict, month_num, name
-        if event_dict is None:
-            self.event_dict = {}
+    def __init__(self, month):
+        self.month = month
+        self._index = -1
+        self.lst = []
+
+    def add(self, events):
+        for event in events:
+            self.lst.append(event)
+
+    def sum(self):
+        pass
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        self._index += 1
+        if self._index >= len(self.lst):
+            self._index = -1
+            raise StopIteration
         else:
-            self.event_dict = event_dict.copy()
-        self.month_num = month_num
-        self.name = map_to_name(month_num)
-
-    def add(self, event_name):
-        if event_name is not None:
-            try:
-                self.event_dict[event_name.upper()] += 1
-                return "Success"
-            except:  # eventually print error to log file here
-                if map_to_alias(event_name.upper()) is not None:
-                    self.event_dict[map_to_alias(event_name.upper())] += 1
-                    return "Success"
-                else:
-                    return event_name, self.month_num
-        else:
-            return "Success"
-
-    def get_name(self):
-        return self.name
-
-    def get_num(self):
-        return self.month_num
+            return self.lst[self._index]
 
     def __str__(self):
-        return f"Month: {self.month_num} ({self.name})\nEvents: {self.event_dict}"
+        return f"{self.month} : {[event.name for event in self.lst]}"
