2a1344dbcf08b4a9a8f6142b779cb420f77455b2
rowleynt <116468465+rowleynt@users.noreply.github.com>
2023-03-18 01:01:43 -0400

    Update event_counter.py

--- event_counter.py
+++ event_counter.py
@@ -1,24 +1,16 @@
 import os
-from Calendar import Calendar
 from OutputHandler import OutputHandler
-from wecutils import read_xlsx, setup, write_log
+from wecutils import read_xlsx, write_log, read_alias_file
 
 
 def main():
-    fmain = os.getcwd()
-    if os.path.exists(".\\data"):
-        os.chdir(".\\data")  # change working directory to 'data' folder
-        files = os.listdir()  # list of all files in 'data' folder
-        calendar = Calendar()
-        for file in files:  # do for every file found within 'data' folder
-            if ".xlsx" in file:  # checks for valid xlsx file
-                read_xlsx(file, calendar)
-        print(OutputHandler(calendar))
-        write_log()
-    else:
-        setup(fmain)
-        print("Directories generated. Place .xlsx file(s) into 'data' folder and edit 'event.txt' and 'aliases.txt' in the"
-              " 'events' folder before rerunning.")
+    os.chdir(".\\data")  # change working directory to 'data' folder
+    files = os.listdir()  # list of all files in 'data' folder
+    aliases = read_alias_file()
+    data = read_xlsx([file for file in files if ".xlsx" in file], aliases)
+    print(OutputHandler(data))
+
+    # write_log()
 
 
 if __name__ == "__main__":
