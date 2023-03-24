8940b48c1ea2b9449b5b8b3f89b422aa7665072c
rowleynt <116468465+rowleynt@users.noreply.github.com>
2023-03-24 11:52:52 -0400

    Update event_counter.py

--- event_counter.py
+++ event_counter.py
@@ -4,7 +4,7 @@ from wecutils import read_xlsx, write_log, read_alias_file
 
 
 def main():
-    os.chdir(".\\data")  # change working directory to 'data' folder
+    os.chdir("data")  # change working directory to 'data' folder
     files = os.listdir()  # list of all files in 'data' folder
     aliases = read_alias_file()
     data = read_xlsx([file for file in files if ".xlsx" in file], aliases)
